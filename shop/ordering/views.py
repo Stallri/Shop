992ba1c984aaa6c462bae0cd630ad9c62ce1f80b
Roman <d4vegame@gmail.com>
2024-02-18 22:26:06 +0300

    order app optimization

--- shop/ordering/views.py
+++ shop/ordering/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import permission_classes
@@ -5,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from products.models import Product
 from .models import Order
 from .serializer import OrderSerializer
 
@@ -14,14 +16,16 @@ from .serializer import OrderSerializer
 class OrderAPI(APIView):
     @extend_schema(summary="Получение списка заказов")
     def get(self, request):
-        queryset = Order.objects.filter(user=request.user)
+        queryset = Order.objects.filter(user=request.user).prefetch_related(
+            Prefetch('products__product',
+                     queryset=Product.objects.select_related('category', 'discount').prefetch_related('photos')))
         serializer = OrderSerializer(instance=queryset, many=True)
         return Response(serializer.data)
 
     @extend_schema(summary="Создание заказа")
     def post(self, request):
         if (request.user.orders.first()) and (not request.user.orders.first().paid):
-            return Response({'error': 'Order not paid'})
+            return Response({'error': 'Last order not paid'})
         serializer = OrderSerializer(data=request.data, context={'user': request.user})
         serializer.is_valid(raise_exception=True)
         serializer.save()
